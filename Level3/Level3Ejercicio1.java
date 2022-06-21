4457c9d60b535bfd21cfb856ee8db97230115db2
melanirodriguez1 <melani.d.rodriguez@outlook.com>
2022-06-21 14:47:23 -0300

    Filtrar lista de String

---
+++ Level3/Level3Ejercicio1.java
@@ -0,0 +1,22 @@
+/*Dada una lista que contiene valores de tipo String, se deberá ﬁltrar todos los
+valores que NO sean null o vacío (""). */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Level3Ejercicio1 {
+    public static void main(String[] args) {
+        List<String> palabras = new ArrayList<>();
+        palabras.add("Hola");
+        palabras.add(null);
+        palabras.add("Informatorio");
+        palabras.add("");
+        List<String> salida = palabras.stream()
+                                     .filter(Objects::nonNull)
+                                     .filter(p -> !p.equals(""))
+                                     .collect(Collectors.toList());
+        System.out.println(salida);
+    }
+}
