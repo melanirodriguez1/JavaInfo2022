a5cc2e792565dd8ab858e2601221cf188ac7fdc8
melanirodriguez1 <melani.d.rodriguez@outlook.com>
2022-06-23 14:57:18 -0300

    Ejercicio 3

---
+++ Level3/Level3Ejercicio4.java
@@ -0,0 +1,22 @@
+/*Se dispone de una lista de Integer, de la cual queremos obtener otra lista
+aplicando la operación de factorial pero no se desean valores repetidos*/
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Level3Ejercicio4 {
+    public static void main (String args[]) {
+        List<Integer> palabras = List.of(1, 2, 4, 4, 4);
+        List<Integer> otraLista = palabras.stream()
+                .map(p -> Level3Ejercicio4.factorial(p))
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println(otraLista);
+    }
+    public static Integer factorial(Integer numero) {
+        if (numero == 1){
+            return 1;
+        }else {
+            return numero * factorial(numero-1);
+        }
+    }
+}
